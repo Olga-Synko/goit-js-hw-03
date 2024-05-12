0106250225242680d0c752e75f1b2c1e1763bbf4
Olga <sinkolga2233@gmail.com>
2024-05-12 14:41:15 +0200

    new file:   .vscode/settings.json 	modified:   index.html 	modified:   js/task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
 function filterArray(numbers, value) {
     const filteredArray = [];
-    for (let i = 0; i < numbers.length; +i) {
-        if (numbers[i] > value) {
-            filteredArray.push(numbers[i]);
-        }
-    }
+
+    for (let elemt of numbers) {
+             if (elemt > value) {
+            filteredArray.push(elemt);
+        }}
     return filteredArray;
 }
+
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
